2375427f4d203847f164afa54ff0d7cf44025a65
Maxwell Silva <maxwell.ssilvva@gmail.com>
2023-02-03 20:03:28 -0400

    Initial commit

---
+++ AppMovies/Controllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppMovies
+//
+//  Created by Maxwell da Silva e Silva on 03/02/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
